9cf6c82a0b7393c9a4c839af5ffef686b10125e1
Penisoara Bogdan <penisoarabogdan@gmail.com>
2018-10-29 21:55:24 +0200

    Modificare carti-jucator si adaugare carti-speciale

--- backend/assets/carti-jucator.js
+++ backend/assets/carti-jucator.js
@@ -61,7 +61,8 @@ module.exports = {
 			"attack": 9,
 			"defence": 3,
 			"club": "FC Barcelona",
-			"country": "Argentina"
+			"country": "Argentina",
+			"image": "https://s2.glbimg.com/-ls_b8vBIdUVepJafhf8HDxaXIU=/560x350/e.glbimg.com/og/ed/f/original/2018/06/12/messi.jpg"
 		},
 		{
 			"name": "COUTINHO",
@@ -69,7 +70,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 6,
 			"club": "FC Barcelona",
-			"country": "Brazil"
+			"country": "Brazil",
+			"image": "https://images.performgroup.com/di/library/GOAL/79/d1/philippe-coutinho-barcelona_7f3t8dkjng8v1509tlidkebaz.jpg?t=1143174161&quality=90&w=1280"
 		},
 		{
 			"name": "RAKITIC",
@@ -77,7 +79,8 @@ module.exports = {
 			"attack": 6,
 			"defence": 6,
 			"club": "FC Barcelona",
-			"country": "Croatia"
+			"country": "Croatia",
+			"image": "https://cdn.images.express.co.uk/img/dynamic/67/590x/Barcelona-news-Ivan-Rakitic-Manchester-United-939217.jpg"
 		},
 		{
 			"name": "POGBA",
@@ -85,7 +88,8 @@ module.exports = {
 			"attack": 5,
 			"defence": 3,
 			"club": "Manchester United",
-			"country": "France"
+			"country": "France",
+			"image": "https://img.rasset.ie/000fa11f-800.jpg"
 		},
 		{
 			"name": "KANTE",
@@ -93,7 +97,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 8,
 			"club": "Chelsea FC",
-			"country": "France"
+			"country": "France",
+			"image": "https://e2.365dm.com/18/07/768x432/skysports-ngolo-kante-chelsea_4374141.jpg?20180728163208"
 		},
 		{
 			"name": "OZIL",
@@ -101,7 +106,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 5,
 			"club": "Arsenal FC",
-			"country": "Germany" 
+			"country": "Germany",
+			"image": "https://images.cdn.fourfourtwo.com/sites/fourfourtwo.com/files/styles/image_landscape/public/mesut_ozil_2.jpg?itok=6xNPoglx"
 		},
 		{
 			"name": "PUYOL",
@@ -109,7 +115,8 @@ module.exports = {
 			"attack": 3,
 			"defence": 10,
 			"club": "FC Barcelona",
-			"country": "Spain" 
+			"country": "Spain",
+			"image": "https://www.thinkingheads.com/wp-content/uploads/2018/03/carlespuyol.jpg"
 		},
 		{
 			"name": "MULLER" ,
@@ -117,7 +124,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 5,
 			"club": "FC Bayern Munchen",
-			"country": "Germany"
+			"country": "Germany",
+			"image": "https://cdn.images.dailystar.co.uk/dynamic/58/photos/59000/620x/Thomas-Muller-646201.jpg"
 		},
 		{
 			"name": "ASENSIO",
@@ -125,7 +133,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 6,
 			"club": "Real Madrid CF",
-			"country": "Spain"
+			"country": "Spain",
+			"image": "https://cdn.images.express.co.uk/img/dynamic/67/590x/Real-Madrid-Neymar-Transfer-Marco-Asensio-890277.jpg"
 		},
 		{
 			"name": "SHAQIRI",
@@ -133,7 +142,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 7,
 			"club": "Stoke City FC",
-			"country": "Switzerland"
+			"country": "Switzerland",
+			"image": "https://images.performgroup.com/di/library/GOAL/31/56/xherdan-shaqiri-liverpool-2018-19_ecooaeeszk411cp0qgc369zou.jpg?t=24470686&quality=90&w=1280"
 		},
 		{
 			"name": "BENTANCUR",
@@ -141,7 +151,8 @@ module.exports = {
 			"attack": 6,
 			"defence": 5,
 			"club": "Juventus FC",
-			"country": "Uruguay"
+			"country": "Uruguay",
+			"image": "https://images.performgroup.com/di/library/GOAL/36/bc/rodrigo-bentancur-juventus-serie-a-23092017_smm3mr99outw1045ji8oq790e.jpg?t=-1174254706&quality=90&w=1280"
 		},
 		{
 			"name": "OSPINA",
@@ -149,7 +160,8 @@ module.exports = {
 			"attack": 3,
 			"defence": 8,
 			"club": "Arsenal FC",
-			"country": "Colombia"
+			"country": "Colombia",
+			"image": "https://metrouk2.files.wordpress.com/2018/08/gettyimages-1013330414.jpg?quality=80&strip=all&zoom=1&resize=644%2C476"
 		},
 		{
 			"name": "VARGAS",
@@ -157,7 +169,8 @@ module.exports = {
 			"attack": 5,
 			"defence": 5,
 			"club": "AC Deportivo Cali",
-			"country": "Colombia"
+			"country": "Colombia",
+			"image": "http://static.hsbnoticias.com/sites/default/files/styles/original/public/gallery/2018/09/camilovargasr.jpg?itok=Ooxew8ZD"
 		},
 		{
 			"name": "OSAMA",
@@ -165,7 +178,8 @@ module.exports = {
 			"attack": 2,
 			"defence": 10,
 			"club": "Al Hilal SFC",
-			"country": "Saudi Arabia" 
+			"country": "Saudi Arabia",
+			"image": "http://worldcup.sportmatch.com/images/web/players/saudi-arabia/osama-hawsawi.jpg"
 		},
 		{
 			"name": "SMOLOV",
@@ -173,7 +187,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 5,
 			"club": "FC Krasnodar",
-			"country": "Russia"
+			"country": "Russia",
+			"image": "https://img.fifa.com/image/upload/t_l4/rvu3yyvly4oxawinor2g.jpg"
 		},
 		{
 			"name": "NACHO",
@@ -181,7 +196,8 @@ module.exports = {
 			"attack": 3,
 			"defence": 7,
 			"club": "Real Madrid CF",
-			"country": "Spain"
+			"country": "Spain",
+			"image": "https://cdn.images.express.co.uk/img/dynamic/67/590x/Nacho-682016.jpg"
 		},
 		{
 			"name": "JOHNSSON",
@@ -189,7 +205,8 @@ module.exports = {
 			"attack": 1,
 			"defence": 8,
 			"club": "EA Guingamp",
-			"country": "Sweden"
+			"country": "Sweden",
+			"image": "https://static.actu.fr/uploads/2016/11/kalle-johnsson-guingamp-eag.jpg"
 		},
 		{
 			"name": "KHALIFA",
@@ -197,7 +214,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 4,
 			"club": "Club Africain",
-			"country": "Tunisia"
+			"country": "Tunisia",
+			"image": "https://www.africatopsports.com/wp-content/uploads/2018/07/SABER-710x457.jpg"
 		},
 		{
 			"name": "CACERES",
@@ -205,7 +223,8 @@ module.exports = {
 			"attack": 3,
 			"defence": 6,
 			"club": "SS Lazio",
-			"country": "Uruguay"
+			"country": "Uruguay",
+			"image": "https://www.thepeninsulaqatar.com/uploads/2018/01/08/post_main_cover_fit//7e774a7872c7a801be5976a01c7c3d49c1f5c326.jpg"
 		},
 		{
 			"name": "ALTPOPE",
@@ -213,7 +232,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 2,
 			"club": "Burnley FC",
-			"country": "England"
+			"country": "England",
+			"image": "https://www.thairath.co.th/media/T18CT60ObSbLrsQ6mSbsNTEhx39rdvZpMA5H0fKBx6NV65gL3KVoEJL.jpg"
 		},
 		{
 			"name": "PICKFORD",
@@ -221,7 +241,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 7,
 			"club": "Everton FC",
-			"country": "England"
+			"country": "England",
+			"image": "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F8cb5cd0a-6753-11e8-996c-7589300f03c9.jpg?crop=1432%2C805%2C614%2C101&resize=685"
 		},
 		{
 			"name": "FELLAINI",
@@ -229,7 +250,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 6,
 			"club": "Manchester United",
-			"country": "Belgium"
+			"country": "Belgium",
+			"image": "https://e1.365dm.com/17/11/768x432/skysports-football-marouane-fellaini-manchester-united_4164587.jpg?20171124064111"
 		},
 		{
 			"name": "NEYMAR JR",
@@ -237,7 +259,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 1,
 			"club": "PSG",
-			"country": "Brazil"
+			"country": "Brazil",
+			"image": "https://static.posters.cz/image/750/postere/fc-barcelona-neymar-jr-i28668.jpg"
 		},
 		{
 			"name": "MARCELO",
@@ -245,7 +268,8 @@ module.exports = {
 			"attack": 3,
 			"defence": 10,
 			"club": "Real Madrid CF",
-			"country": "Brazil" 
+			"country": "Brazil",
+			"image": "https://as01.epimg.net/futbol/imagenes/2018/10/25/primera/1540486002_091722_1540486102_noticia_normal.jpg"
 		},
 		{
 			"name": "VERMAELEN",
@@ -253,7 +277,8 @@ module.exports = {
 			"attack":5 ,
 			"defence": 9,
 			"club": "FC Barcelona",
-			"country": "Spain"
+			"country": "Spain",
+			"image": "https://cdn.vox-cdn.com/thumbor/VQ-H8UF9_nN1c9GUoAtX_YsGgEM=/0x100:2660x1873/1200x800/filters:focal(0x100:2660x1873)/cdn.vox-cdn.com/uploads/chorus_image/image/41359066/453431032.0.jpg"
 		},
 		{
 			"name": "POPE",
@@ -261,7 +286,8 @@ module.exports = {
 			"attack": 2,
 			"defence": 10,
 			"club": "Burnley FC",
-			"country": "England"
+			"country": "England",
+			"image": "http://wsbuzz.com/wp-content/uploads/2018/03/nintchdbpict000392433290.jpg"
 		},
 		{
 			"name": "SCHRAM",
@@ -269,7 +295,8 @@ module.exports = {
 			"attack": 3,
 			"defence": 8,
 			"club": "Roskilde BK",
-			"country": "Iceland"
+			"country": "Iceland",
+			"image": "https://scontent-lax3-2.cdninstagram.com/vp/8abbde6afd255126276e83b945b7d5fe/5C5BA91A/t51.2885-15/sh0.08/e35/s640x640/39613387_2140390719563085_7612705931591155712_n.jpg"
 		},
 		{
 			"name": "CUADRADO",
@@ -277,7 +304,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 4,
 			"club": "CD Once Caldas",
-			"country": "Colombia"
+			"country": "Colombia",
+			"image": "https://cdn.images.express.co.uk/img/dynamic/67/590x/Arsenal-news-Juan-Cuadrado-857344.jpg"
 		},
 		{
 			"name": "PEMBERTON",
@@ -285,7 +313,8 @@ module.exports = {
 			"attack": 1,
 			"defence": 3,
 			"club": "Costa Rica",
-			"country": "LD Alajuelense"
+			"country": "LD Alajuelense",
+			"image": "https://s.hs-data.com/bilder/spieler/gross/172614.jpg"
 		},
 		{
 			"name": "MOREIRA",
@@ -293,7 +322,8 @@ module.exports = {
 			"attack": 6,
 			"defence": 4,
 			"club": "CS Herediano",
-			"country": "Costa Rica"
+			"country": "Costa Rica",
+			"image": "https://www.diez.hn/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=xvQQiHWO_UAxKTKNExUAzs$daE2N3K4ZzOUsqbU5sYvMVA83pdMYzLe0vcbHxtNiWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&CONTENTTYPE=image/jpeg"
 		},
 		{
 			"name": "LIVAKOVIC",
@@ -301,7 +331,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 4,
 			"club": "GNK Dinamo Zagreb",
-			"country": "Croatia"
+			"country": "Croatia",
+			"image": "https://i.pinimg.com/originals/dc/b2/32/dcb232179a14516673b34fae56c6800f.jpg"
 		},
 		{
 			"name": "SCHMEICHEL",
@@ -309,7 +340,8 @@ module.exports = {
 			"attack": 4,
 			"defence": 9,
 			"club": "Leicester City FC",
-			"country": "Denmark"
+			"country": "Denmark",
+			"image": "https://e2.365dm.com/17/03/768x432/skysports-kasper-schmeichel-leicester-city-premier-league-football_3913799.jpg?20170321102553"
 		},
 		{
 			"name": "ELHADARY",
@@ -317,7 +349,8 @@ module.exports = {
 			"attack": 2,
 			"defence": 8,
 			"club": "Al Taawoun FC",
-			"country": "Egypt"
+			"country": "Egypt",
+			"image": "https://www.sofoot.com/IMG/img_580_380_center_articles-437797.jpg"
 		},
 		{
 			"name": "ROJO",
@@ -325,7 +358,8 @@ module.exports = {
 			"attack": 4,
 			"defence": 8,
 			"club": "Manchester United",
-			"country": "England" 
+			"country": "England",
+			"image": "https://cdn.images.dailystar.co.uk/dynamic/58/photos/315000/620x/Marcos-Rojo-589428.jpg"
 		},
 		{
 			"name": "GIROUD",
@@ -333,7 +367,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 3,
 			"club": "Chelsea",
-			"country": "France"
+			"country": "France",
+			"image": "http://dailypost.ng/wp-content/uploads/2018/02/gi.png"
 		},
 		{
 			"name": "GRIEZMANN",
@@ -341,7 +376,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 4,
 			"club": "Atletico Madrid",
-			"country": "France"
+			"country": "France",
+			"image": "http://as01.epimg.net/futbol/imagenes/2017/06/04/primera/1496570307_204106_1496570423_noticia_normal.jpg"
 		},
 		{
 			"name": "STERLING",
@@ -349,7 +385,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 3,
 			"club": "Manchester City",
-			"country": "England"
+			"country": "England",
+			"image": "https://images.performgroup.com/di/library/GOAL/93/48/raheem-sterling-manchester-city_bbxgewa8k6lm15jk5wnzre51h.jpg?t=573236289&quality=90&w=1280"
 		},
 		{
 			"name": "WELBECK",
@@ -357,7 +394,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 5,
 			"club": "Arsenal",
-			"country": "England"
+			"country": "England",
+			"image": "https://s.hs-data.com/bilder/spieler/gross/113762.jpg"
 		},
 		{
 			"name": "FISCHER",
@@ -365,7 +403,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 5,
 			"club": "FC Kobenhavn",
-			"country": "Denmark"
+			"country": "Denmark",
+			"image": "https://www.fck.dk/sites/default/files/styles/article_full/public/images/2018-02/180210_fck-rfc_09.jpg?itok=5Fodi1uB"
 		},
 		{
 			"name": "PJACA",
@@ -373,7 +412,8 @@ module.exports = {
 			"attack": 8,
 			"defence": 4,
 			"club": "Schalke 04",
-			"country": "Croatia"
+			"country": "Croatia",
+			"image": "https://i.eurosport.com/2016/07/21/1897929-39954400-2560-1440.jpg?w=1050"
 		},
 		{
 			"name": "BACCA",
@@ -381,7 +421,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 4,
 			"club": "Villarreal CF",
-			"country": "Colombia"
+			"country": "Colombia",
+			"image": "https://www.foottheball.com/wp-content/uploads/2016/07/Carlos-Bacca-AC-Milan.jpg"
 		},
 		{
 			"name": "HIGUAIN" ,
@@ -389,7 +430,8 @@ module.exports = {
 			"attack": 7,
 			"defence": 4,
 			"club": "Juventus",
-			"country": "Argentina" 
+			"country": "Argentina",
+			"image": "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2018/05/18/15266536060009.jpg"
 		}
 	]
 }
---
+++ backend/assets/carti-speciale.js
@@ -0,0 +1,45 @@
+module.exports = {
+
+	cards : [
+		{
+            "name": "Yellow card",
+            "description": "Halves points of one opponent's card"   
+        },
+        {
+            "name": "Red card",
+            "description": "Reduce to 0 defence and attack points of a card"
+        },
+        {
+            "name": "Player transfer",
+            "description": "Switch between one of your card and your opponent's card" 
+        },
+        {
+            "name": "Injury",
+            "description": "Remove one of your opponent's card from table"
+        },
+        {
+            "name": "Switch",
+            "description": "Switch between two cards, one from table and one from deck"
+        },
+        {
+            "name": "Doping",
+            "description": "Adds to a card half of its value (attack and defence)"
+        },
+        {
+            "name": "Antidoping-test",
+            "description": "Remove all doping effects from table"
+        },
+        {
+            "name": "Video assistant",
+            "description": "Remove all Yellow and Red cards from a card"
+        },
+        {
+            "name": "Fifty-fifty",
+            "description": "50% chances to increase or decrease 2 points to defence and attack"
+        },
+        {
+            "name": "Fog",
+            "description": "Decrease all cards points, of both players, placed on attack position to 0"
+        }
+	]
+}
