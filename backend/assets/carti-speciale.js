9793d70f1724941a92662a18de1ce7fa6019e46a
AdrianStefan23 <adrian.cosmin23@gmail.com>
2018-11-04 19:05:25 +0200

    Show board, show cards in hand and show info done (#22)

--- backend/assets/carti-speciale.js
+++ backend/assets/carti-speciale.js
@@ -3,43 +3,53 @@ module.exports = {
     cards : [
         {
             "name": "Yellow-card",
-            "description": "Halves points of one opponent's card"
+            "description": "Halves points of one opponent's card",
+            "miniCard": "https://imgur.com/arrmKrp.png"
         },
         {
             "name": "Red-card",
-            "description": "Reduce to 0 defence and attack points of a card"
+            "description": "Reduce to 0 defence and attack points of a card",
+            "miniCard": "https://imgur.com/qS1qH9W.png"
         },
         {
             "name": "Player-transfer",
-            "description": "Switch between one of your card and your opponent's card"
+            "description": "Switch between one of your card and your opponent's card",
+            "miniCard": "https://imgur.com/DGWh0FA.png"
         },
         {
             "name": "Injury",
-            "description": "Remove one of your opponent's card from table"
+            "description": "Remove one of your opponent's card from table",
+            "miniCard": "https://imgur.com/IjGUG0H.png"
         },
         {
             "name": "Switch",
-            "description": "Switch between two cards, one from table and one from deck"
+            "description": "Switch between two cards, one from table and one from deck",
+            "miniCard": "https://imgur.com/thY9PuU.png"
         },
         {
             "name": "Doping",
-            "description": "Adds to a card half of its value (attack and defence)"
+            "description": "Adds to a card half of its value (attack and defence)",
+            "miniCard": "https://imgur.com/bYqfRC4.png"
         },
         {
             "name": "Antidoping-test",
-            "description": "Remove all doping effects from table"
+            "description": "Remove all doping effects from table",
+            "miniCard": "https://imgur.com/ePPEWq6.png"
         },
         {
             "name": "Video-assistant",
-            "description": "Remove all Yellow and Red cards from a card"
+            "description": "Remove all Yellow and Red cards from a card",
+            "miniCard": "https://imgur.com/NIGp4I2.png"
         },
         {
             "name": "Fifty-fifty",
-            "description": "50% chances to increase or decrease 2 points to defence and attack"
+            "description": "50% chances to increase or decrease 2 points to defence and attack",
+            "miniCard": "https://imgur.com/8Z6Pb8T.png"
         },
         {
             "name": "Fog",
-            "description": "Decrease all cards points, of both players, placed on attack position to 0"
+            "description": "Decrease all cards points, of both players, placed on attack position to 0",
+            "miniCard": "https://imgur.com/ylYKlyw.png"
         }
     ]
 }
